c0165627de366378f301aff340d4560cde387861
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-07-16 15:07:17 -0700

    refactor user test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -40,9 +40,8 @@ RSpec.describe User, type: :model do
     end
 
     it 'email is unique and not case sensitive and does not care white space before and after' do
-      @user1 = User.new(first_name: 'f-name', last_name: 'l-name', email: 'a@g.com', password: '123', password_confirmation: '123')
-      expect(@user1).to be_valid
-      @user1.save
+      @user1 = User.create(first_name: 'f-name', last_name: 'l-name', email: 'a@g.com', password: '123', password_confirmation: '123')
+
       @user2 = User.new(first_name: 'f-name', last_name: 'l-name', email: ' A@g.com', password: '123', password_confirmation: '123')
       expect(@user2).to_not be_valid
     end
@@ -56,36 +55,28 @@ RSpec.describe User, type: :model do
   describe '.authenticate_with_credentials' do
 
     it 'will return user if user exist' do
-      @user = User.new(first_name: 'f-name', last_name:'l-name', email:'a@g.com', password: '123', password_confirmation: '123')
-      expect(@user).to be_valid
-      @user.save
+      @user = User.create(first_name: 'f-name', last_name:'l-name', email:'a@g.com', password: '123', password_confirmation: '123')
 
       @loginUser = User.authenticate_with_credentials 'a@g.com', '123'
       expect(@user).to match @loginUser
     end
 
     it 'will return nil if user not exist' do
-      @user = User.new(first_name: 'f-name', last_name:'l-name', email:'a@g.com', password: '123', password_confirmation: '123')
-      expect(@user).to be_valid
-      @user.save
+      @user = User.create(first_name: 'f-name', last_name:'l-name', email:'a@g.com', password: '123', password_confirmation: '123')
 
       @loginUser = User.authenticate_with_credentials 'b@g.com', '123'
       expect(@loginUser).to be_nil
     end
 
     it 'will match user with white space around email' do
-      @user = User.new(first_name: 'f-name', last_name:'l-name', email:'a@g.com', password: '123', password_confirmation: '123')
-      expect(@user).to be_valid
-      @user.save
+      @user = User.create(first_name: 'f-name', last_name:'l-name', email:'a@g.com', password: '123', password_confirmation: '123')
 
       @loginUser = User.authenticate_with_credentials ' a@g.com ', '123'
       expect(@user).to match @loginUser
     end
 
     it 'will match user with email case insensitive' do
-      @user = User.new(first_name: 'f-name', last_name:'l-name', email:'a@g.com', password: '123', password_confirmation: '123')
-      expect(@user).to be_valid
-      @user.save
+      @user = User.create(first_name: 'f-name', last_name:'l-name', email:'a@g.com', password: '123', password_confirmation: '123')
 
       @loginUser = User.authenticate_with_credentials 'A@g.com', '123'
       expect(@user).to match @loginUser
